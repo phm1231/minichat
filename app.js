0d174986adc0a9bf5c5da45b97809f2e12326fd1
San <jtlsan@naver.com>
2020-11-24 18:09:06 +0900

    Initial settings

---
+++ app.js
@@ -0,0 +1,11 @@
+var connect = require('connect')
+var http = require('http')
+var fs = require('fs')
+var app = connect()
+
+function index(request, response) {
+    fs.createReadStream('./views/index.ejs').pipe(response)
+}
+
+app.use('/', index);
+http.createServer(app).listen(8000);
