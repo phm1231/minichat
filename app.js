3c589b38a4dd6b1ea6153222e9d61a25f8660cf4
phm1231 <157257@naver.com>
2020-12-06 19:51:32 -0800

    maked branch chat_2 for merge

--- app.js
+++ app.js
@@ -6,7 +6,29 @@ var app = express()
 
 var indexRouter = require('./routes/index')
 
+const server = http.createServer(app);
+const io = require('socket.io')(server);
+var port = 8000;
+
+server.listen(port, ()=>{
+		console.log('listening on port : %d', port);
+		});
+
+app.get('/', (req, res)=>{
+		res.sendFile(__dirname + '/views/index.html');
+		});
+
+io.on('connection', (socket)=>{
+		console.log('user_connected');
+		socket.on('disconnect', ()=>{
+				console.log('user_disconnected');
+				});
+		socket.on('chat-msg', (msg)=>{
+				io.emit('chat-msg', msg);
+				});
+});
+
 app.use('/', indexRouter)
 
 
-http.createServer(app).listen(8000);
+//http.createServer(app).listen(8000);
