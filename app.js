0b02c7d48d78193fad92bfedf32db6ceec668619
San <jtlsan@naver.com>
2020-12-07 13:17:00 +0900

    tmp

--- app.js
+++ app.js
@@ -8,11 +8,12 @@ var routes = require('./routes');
 var bodyParser = require("body-parser");
 
 var indexRouter = require('./routes/index')
-
+var signupRouter = require('./routes/signup');
 app.set("views", path.join(__dirname,"views"));
 app.set("view engine", "ejs");
 
 app.use("/", indexRouter)
+app.use('/signup/', signupRouter);
 
 
 http.createServer(app).listen(8000);
